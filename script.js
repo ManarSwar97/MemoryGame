6aeebeb74be05f0f9fed1a1d19fed7fe422b3a50
ManarSwar97 <manarswar97@gmail.com>
2025-04-23 22:44:54 +0300

    Memory Game

---
+++ script.js
@@ -0,0 +1,25 @@
+//html pseudocode
+//HTML
+//create two html pages
+//the first page contains: title of the game - instruction - a start button that'll navigate to the game page
+//the second page contains: a timer - a score that'll calculate how many times the player wins - cards
+
+
+
+
+//CSS
+//we need to use grid to align the cards in the page
+//style the pages
+//add flipping animation for the cards when they flipped 
+
+
+
+//Javascript
+//when the game start the timer will start and the cards should be flipped and randomized
+//the user should be able to find all matches cards to win
+//if the user select two unmatched cards they'll be flipped again
+//if the user select two matched cards they'll appear
+//if the user didn't find all the matching cards within the time required it'll show a message that he lose
+//if the user find all the matching cards within the time required it'll show a message that he win
+//if the user win it'll increase the store and show an option if he want to continue playing the game
+
