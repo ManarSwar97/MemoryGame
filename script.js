b19f9e374e194b3c67d50974b01269c5c3ba9c08
ManarSwar97 <165576868+ManarSwar97@users.noreply.github.com>
2025-04-24 10:20:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
 //when the game start the timer will start and the cards should be flipped and randomized
 //the user should be able to find all matches cards to win
 //if the user select two unmatched cards they'll be flipped again
-//if the user select two matched cards they'll appear
+//if the user select two matched cards they'll display
 //if the user didn't find all the matching cards within the time required it'll show a message that he lose
 //if the user find all the matching cards within the time required it'll show a message that he win
 //if the user win it'll increase the store and show an option if he want to continue playing the game
